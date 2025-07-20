ff7b08a1f098421e61a89a3eaef20a0c3069d860
amadeo <risefenixpuryfire@gmail.com>
2025-07-21 01:36:46 +0300

    api -> apito to manage errors with strapi on server

--- src/routes/chess.py
+++ src/routes/chess.py
@@ -7,7 +7,7 @@ from models.chess_fsr_rating_calculator import FCROutput, FCRInput
 from services.chess_service import ChessService
 
 
-router = APIRouter(prefix="/api/chess", tags=["chess"])
+router = APIRouter(prefix="/apito/chess", tags=["chess"])
 
 @router.post("/calculate-rating-fide/", response_model=ChessFIDEOutput)
 async def get_chess(input_data: ChessFIDEInput) -> ChessFIDEOutput:
